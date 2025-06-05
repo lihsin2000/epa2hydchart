f5682a12c6bcb8427fb6edbfc8752ec2efabd048
lihsin2000 <lihsin2000@gmail.com>
2025-06-05 16:21:55 +0800

    update

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ joint_scale=25
 text_size=25.0
 leader_distance=50.0
 
-block_scale_default=100
-joint_scale_default=25
+block_scale_default=100.0
+joint_scale_default=25.0
 text_size_default=25.0
 leader_distance_default=50.0
 
